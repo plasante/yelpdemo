2c56170df40caf30294cfc10f31ee3309df24bb0
Pierre Lasante <pierre.lasante@videotron.ca>
2019-01-20 08:57:00 -0500

    Add data validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,18 @@ class Restaurant < ApplicationRecord
 	mount_uploader :image, ImageUploader
 
 	has_many :reviews
+
+	validates :name, :address, :phone, :website, :image, presence: true
+
+	validates :phone, format: { with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
+      messages: "must be in format (123) 456-7890" 
+    }
+
+    validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+      messages: "must start with http:// or https://" 
+    }
+
+    validates :address, format: { with: /\A\d+[^,]+,[^,]+, [A-Z]{2} [a-zA-Z0-9 ]{5,7}\z/,
+      messages: "must be in the format 25 chemin Ste-Marie, Mascouche, PQ, J7J 1M8" 
+    }
 end
